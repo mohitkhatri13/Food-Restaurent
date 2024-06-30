55830f815a966165b4a835054b6aa92b4d28c949
mohit khatri <khatrimohit2002@gmail.com>
2024-06-30 20:47:32 +0530

    scripts

--- routes/categoryroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createCategory , getCategories} = require('../controllers/categoryController');
-
-const router = express.Router();
-
-router.post('/categories', createCategory);
-router.get("/getcategories" , getCategories)
-
-module.exports = router;
