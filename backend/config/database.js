84c1f8a481df8229d4d0959a61e929ecc1687b93
mohit khatri <khatrimohit2002@gmail.com>
2024-06-30 21:02:18 +0530

    env

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const DB_NAME ="newbackend";
 require("dotenv").config();
 const connectDb = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
