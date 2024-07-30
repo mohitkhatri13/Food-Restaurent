e063aef8f2b6b280fceb086079f7153e12659c83
mohit khatri <khatrimohit2002@gmail.com>
2024-07-30 12:16:12 +0530

    db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose")
 // const DB_NAME ="newbackend";
 require("dotenv").config();
-const URL = "mongodb+srv://khatrimohit2002:n2dRSL6GftIZ1Zsw@cluster0.3n8ipdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connectDb = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(URL)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URL)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
