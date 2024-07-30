fe7da709520e21997311930c129314cef8983bee
mohit khatri <khatrimohit2002@gmail.com>
2024-07-30 12:10:36 +0530

    newdb

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
-const DB_NAME ="newbackend";
+// const DB_NAME ="newbackend";
 require("dotenv").config();
+const URL = "mongodb+srv://khatrimohit2002:n2dRSL6GftIZ1Zsw@cluster0.3n8ipdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connectDb = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(URL)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
